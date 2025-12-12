12399bb0683bae1470eeed4682c7c0805a0c55bd
Tiến Đạt DevOps <121950007+tiendat2204@users.noreply.github.com>
2025-12-12 15:44:14 +0700

    Add favicon.ico and update layout.tsx for improved metadata handling and theme color support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import { TooltipProvider } from '@/components/ui/tooltip'
-import { DATA } from '@/data/resume'
+import { DATA, META_THEME_COLORS } from '@/data/resume'
 import { cn } from '@/lib/utils'
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import './globals.css'
 import { ReactLenis } from 'lenis/react'
 import { Footer } from '@/components/footer'
 import { Providers } from '@/components/Providers'
 import { Header } from '@/components/Header'
+import type { WebSite, WithContext } from 'schema-dts'
+import { SITE_INFO } from '@/data/config'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   metadataBase: new URL(DATA.url),
@@ -16,9 +19,20 @@ export const metadata: Metadata = {
   },
   description: DATA.description,
   icons: {
-    icon: '/logo.svg',
-    shortcut: '/logo.svg',
-    apple: '/logo.svg',
+    icon: [
+      {
+        url: '/favicon.ico',
+        sizes: 'any',
+      },
+      {
+        url: '/logo.svg',
+        type: 'image/svg+xml',
+      },
+    ],
+    apple: {
+      url: '/logo.svg',
+      sizes: '180x180',
+    },
   },
   openGraph: {
     title: `${DATA.name}`,
@@ -29,7 +43,7 @@ export const metadata: Metadata = {
     type: 'website',
     images: [
       {
-        url: '/me.jpg',
+        url: `${DATA.url}me.jpg`,
         width: 1200,
         height: 630,
         alt: DATA.name
@@ -56,7 +70,34 @@ export const metadata: Metadata = {
     yandex: '',
   },
 }
+function getWebSiteJsonLd (): WithContext<WebSite> {
+  return {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: SITE_INFO.name,
+    url: SITE_INFO.url,
+    alternateName: [DATA.name],
+  }
+}
+const darkModeScript = String.raw`
+  try {
+    if (localStorage.theme === 'dark' || ((!('theme' in localStorage) || localStorage.theme === 'system') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.querySelector('meta[name="theme-color"]').setAttribute('content', '${META_THEME_COLORS.dark}')
+    }
+  } catch (_) {}
 
+  try {
+    if (/(Mac|iPhone|iPod|iPad)/i.test(navigator.platform)) {
+      document.documentElement.classList.add('os-macos')
+    }
+  } catch (_) {}
+`
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+  themeColor: META_THEME_COLORS.light,
+}
 export default function RootLayout ({
   children,
 }: Readonly<{
@@ -64,6 +105,23 @@ export default function RootLayout ({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
+      <head>
+        <script
+          type='text/javascript'
+          dangerouslySetInnerHTML={{ __html: darkModeScript }}
+        />
+        {/*
+          Thanks @tailwindcss. We inject the script via the `<Script/>` tag again,
+          since we found the regular `<script>` tag to not execute when rendering a not-found page.
+         */}
+        <Script src={`data:text/javascript;base64,${btoa(darkModeScript)}`} />
+        <script
+          type='application/ld+json'
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(getWebSiteJsonLd()).replace(/</g, '\\u003c'),
+          }}
+        />
+      </head>
       <body
         className={cn(
           'min-h-screen bg-background antialiased max-w-3xl mx-auto py-2  md:py-5 mt-2 md:mt-0 overflow-x-hidden px-4'
