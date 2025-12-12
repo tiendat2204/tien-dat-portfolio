9bdba9942c8759cd280d19803dca6d85338b81e3
Tiến Đạt DevOps <121950007+tiendat2204@users.noreply.github.com>
2025-12-12 13:41:31 +0700

    Remove favicon.ico and update layout.tsx to include images in Open Graph metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,14 @@ export const metadata: Metadata = {
     siteName: `${DATA.name}`,
     locale: 'en_US',
     type: 'website',
+    images: [
+      {
+        url: '/me.jpg',
+        width: 1200,
+        height: 630,
+        alt: DATA.name
+      }
+    ]
   },
   robots: {
     index: true,
